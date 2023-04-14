2c94e3ee7692f7b6fa8250bc72796691cff1c165
MNasybullin <mansurnasybullin@gmail.com>
2023-04-14 20:36:18 +0300

    HW_Lecture_3 added

---
+++ Algorithm training 1.0/HW_Lecture_3/HW_Lecture_3/main.swift
@@ -0,0 +1,98 @@
+//
+//  main.swift
+//  HW_Lecture_3
+//
+//  Created by Mansur Nasybullin on 14.04.2023.
+//
+
+import Foundation
+
+// MARK: - A. Количество различных чисел | Status = OK
+
+//let input = readLine()!.split(separator: " ").map{ Int($0)! }
+//var set = Set<Int>()
+//input.forEach { set.insert($0) }
+//print(set.count)
+
+// MARK: - B. Пересечение множеств | Status = OK
+ 
+//let first = readLine()!.split(separator: " ").map { Int($0)! }
+//let second = readLine()!.split(separator: " ").map { Int($0)! }
+//
+//var result = Set<Int>()
+//first.forEach { result.insert($0) }
+//print(result.intersection(second).sorted(by: <).map(String.init).joined(separator: " "))
+
+// MARK: - C. Кубики | Status = OK
+
+//let count = readLine()!.split(separator: " ").map { Int($0)! }
+//
+//var anyaSet = Set<Int>()
+//for _ in 0..<count[0] {
+//    anyaSet.insert(Int(readLine()!)!)
+//}
+//
+//var boryaSet = Set<Int>()
+//for _ in 0..<count[1] {
+//    boryaSet.insert(Int(readLine()!)!)
+//}
+//
+//let first = anyaSet.intersection(boryaSet)
+//print(first.count)
+//print(first.sorted(by: <).map(String.init).joined(separator: " "))
+//
+//let second = anyaSet.subtracting(first)
+//print(second.count)
+//print(second.sorted(by: <).map(String.init).joined(separator: " "))
+//
+//let third = boryaSet.subtracting(first)
+//print(third.count)
+//print(third.sorted(by: <).map(String.init).joined(separator: " "))
+
+// MARK: - D. Количество слов в тексте | Status = OK
+
+//var status = true
+//var text = String()
+//while status {
+//    let input = readLine()
+//    if input == nil || input!.isEmpty {
+//        status = false
+//        break
+//    }
+//    text += " " + input!
+//}
+//var set = Set<String>()
+//text.split(separator: " ").forEach {
+//    set.insert(String($0))
+//}
+//print(set.count)
+
+// MARK: - E. OpenCalculator | Status = OK
+
+//let one = readLine()!.split(separator: " ").map { Int($0)! }
+//
+//var set = Set<Int>()
+//readLine()!.split(separator: "").map{ Int($0)! }.forEach{ set.insert($0) }
+//
+//print(set.subtracting(one).count)
+
+// MARK: - F. Инопланетный геном | Status = OK
+
+//let first = readLine()!.split(separator: "")
+//let second = readLine()!.split(separator: "")
+//var secondSet = Set<String.SubSequence>()
+//for i in 0..<second.count - 1 {
+//    secondSet.insert((second[i] + second[i + 1]))
+//}
+//
+//var count = 0
+//for i in 0..<first.count - 1 {
+//    let pair = first[i] + first[i + 1]
+//    count += secondSet.contains(pair) == true ? 1 : 0
+//}
+//print(count)
+
+
+// MARK: - G. Черепахи | Status =
+
+
